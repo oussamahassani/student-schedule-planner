15a64f82f218a442dc5ca81d313ff12e6b5966b9
oussamahassani <oussamahassanisimplon@gmail.com>
2024-04-24 07:03:09 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmploiController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});
+// Fetch all emplois
+Route::get('/emplois', [EmploiController::class, 'getAllData']);
+
+// Fetch emplois by filiere
+Route::get('/emplois/{filiere}', [EmploiController::class, 'getByFiliere']);
+
+// Fetch emplois by filiere and type
+Route::get('/emplois/{filiere}/{type}', [EmploiController::class, 'getByFiliereAndType']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
